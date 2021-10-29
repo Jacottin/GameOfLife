4157602281f43f0d206771c90359774d9dad953e
Jacottin <49026472+Jacottin@users.noreply.github.com>
2021-10-29 22:08:05 +0200

    Updated classes Cell and Grid

--- GameOfLife/Cell.cs
+++ GameOfLife/Cell.cs
@@ -2,30 +2,41 @@
 {
     public class Cell
     {
-        private bool _isAlive 
-        public bool isAlive { get; set; }
-        private bool _nextState 
-        public bool nextState { get; set; }
+        private bool _isAlive;
+        private bool _nextState;
+        
+        public bool isAlive
+        {
+            get { return _isAlive; }
+            set { _isAlive = value; }
+        }
+
+        public bool nextState
+        {
+            get { return _nextState; }
+            set { _nextState = value; }
+        }
 
 
         public bool Cell(bool state)
         {
-            _isAlive()
+            isAlive = false;
+            return state;
         }
 
         public void ComeAlive()
         {
-            
+            nextState = true;
         }
 
         public void PassAway()
         {
-            
+            nextState = false;
         }
 
         public void Update()
         {
-            
+            isAlive = nextState;
         }
     }
 }
--- GameOfLife/Grid.cs
+++ GameOfLife/Grid.cs
@@ -4,12 +4,29 @@ namespace GameOfLife
 {
     public class Grid
     {
-        private int _n { get; set; }
+        private int _n;
         public Cell[,] TabCells;
+
+        public int n
+        {
+            get { return _n; }
+            set { _n = value; }
+        }
 
         public void Grid(int nbCells, List<Coords> AliveCellsCoords)
         {
-            
+            this.n = nbCells;
+            TabCells = new Cell[n, n];
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    if (expr)
+                    {
+                        
+                    }
+                }
+            }
         }
 
         public int GetNbAliveNeighboor(int i, int j)
